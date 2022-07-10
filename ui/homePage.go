0300267b8dbf69cb71cd6339c741cd125d798dba
Jirawat Boonkumnerd <jo06942@gmail.com>
2022-07-11 00:59:11 +0700

    feat: swap time and data column

--- ui/homePage.go
+++ ui/homePage.go
@@ -15,18 +15,19 @@ func ClipboardBox(cc *crossclipboard.CrossClipboard) tview.Primitive {
 	go func() {
 		for clipboards := range cc.ClipboardManager.ClipboardsChannel {
 			table.Clear()
-			table.SetCell(0, 0, tview.NewTableCell("text").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
-			table.SetCell(0, 1, tview.NewTableCell("size").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignCenter))
-			table.SetCell(0, 2, tview.NewTableCell("time").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignRight))
+			table.SetCell(0, 0, tview.NewTableCell("time").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignRight))
+			table.SetCell(0, 1, tview.NewTableCell("size (bytes)").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignCenter))
+			table.SetCell(0, 2, tview.NewTableCell("data").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+
 			for i, clipboard := range clipboards {
 				row := i + 1
-				if clipboard.Size > 50 {
-					table.SetCell(row, 0, tview.NewTableCell(string(clipboard.Data[:50])))
-				} else {
-					table.SetCell(row, 0, tview.NewTableCell(string(clipboard.Data)))
-				}
+				table.SetCell(row, 0, tview.NewTableCell(clipboard.Time.Format("15:04:05")))
 				table.SetCell(row, 1, tview.NewTableCell(strconv.FormatUint(uint64(clipboard.Size), 10)))
-				table.SetCell(row, 2, tview.NewTableCell(clipboard.Time.Format("15:04:05")))
+				if clipboard.Size > 50 {
+					table.SetCell(row, 2, tview.NewTableCell(string(clipboard.Data[:50])))
+				} else {
+					table.SetCell(row, 2, tview.NewTableCell(string(clipboard.Data)))
+				}
 			}
 		}
 	}()
